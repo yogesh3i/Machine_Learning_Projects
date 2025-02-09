81250a44d539390db418c9bc4947063de540567b
Yogesh <yogideotale33@gmail.com>
2025-02-10 04:52:55 +0530

    folder created

---
+++ template.py
@@ -0,0 +1,43 @@
+import os 
+from pathlib import Path 
+import logging 
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s: %(message)s:')
+project_name = "wine_quality_Peoject"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
+
+]
+
+# with the below code creating this folder and the files in the location 
+for filepth in list_of_files:
+    filrpath = Path(filepath)
+    filedir,filename= os.path.split(filepath)
+    if filedir != "":
+        os.mkdirs(filedir,exist_ok=True)
+        logging.info(f"Creating dirctory; {filedir} for the file:{filename}")
+    if (not os.filepath.exist(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+        logging.info(f"creating empty file:{filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
